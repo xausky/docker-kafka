9e140aed105ded47150a18607dd01e3376a0c720
xausky <xausky@gmail.com>
2016-10-19 15:51:15 +0800

    [FIX]init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo curl -fSL http://www.apache.org/dist/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz -o kafka.tgz
+echo curl -fSL http://www.apache.org/dist/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz -o /kafka.tgz
 
-curl -fSL http://www.apache.org/dist/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz -o kafka.tgz
+curl -fSL http://www.apache.org/dist/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz -o /kafka.tgz
 
-tar -xz -f kafka.tgz
+tar -xzC / -f kafka.tgz
 
-rm -rf kafka.tar.gz
+rm -rf /kafka.tgz
 
-mv kafka* kafka
+mv /kafka* /kafka
 
-cd kafka
+cd /kafka
 
 sed -i 's/zookeeper.connect=localhost:2181/zookeeper.connect=zookeeper:2181/g' config/server.properties
